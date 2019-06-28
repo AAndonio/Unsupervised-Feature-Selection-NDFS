bb634a4b57a11f2bb47c4949ddb7013a635a3f54
Antonio Vivone <ndonio96@gmail.com>
2019-06-28 10:40:08 +0200

    Aggiunto dizionario con num cluster/classi

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ import sys
 
 def main():
 
+    num_cluster_dizionario = {
+        "ECG200": 2,
+        "ECG5000": 5,
+        "FordA": 2,
+        "FordB": 2,
+        "ChlorineConcentration": 3,
+        "PhalangesOutlinesCorrect": 3, 
+        "RefrigerationDevices": 3,
+        "TwoLeadECG": 2,
+        "TwoPatterns": 4
+    }
+
     filename = sys.argv[1]
     num_feature = int(sys.argv[2])
-    num_cluster = int(sys.argv[3])
+    num_cluster = num_cluster_dizionario[filename]
 
     selectRelevantFeatureTSFresh.selectRelevantFeatureTSFresh(filename, num_cluster)
     selectFeatureNDFS.selectFeatureNDFS(filename, num_feature, num_cluster)
